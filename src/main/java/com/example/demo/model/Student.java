1e183ee781106cf5892fa051ca6bc7d2f6d0f0a6
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-03-21 08:44:42 +0530

    creating db and student table in db

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,9 +1,23 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
-
+@Entity
+@Table
 public class Student {
+
+    @Id             // primary key attribute
+    @SequenceGenerator(             // sequence generator is nothing but a kind of incremental value passed to the primary key
+            name = "studentSequence",           // name in db table
+            sequenceName = "studentSequence",           // reference name in in generated value
+            allocationSize = 1              // value by which increment happens after every object creation
+    )
+    @GeneratedValue(            // helps the attribute in giving an auto value on object creation
+            strategy = GenerationType.SEQUENCE,
+            generator = "studentSequence"
+    )
     private Long id;
     private String name;
     private String email;
@@ -23,6 +37,10 @@ public class Student {
         this.email = email;
         this.dob = dob;
         this.age = age;
+    }
+
+    public Student() {
+
     }
 
     public Long getId() {
